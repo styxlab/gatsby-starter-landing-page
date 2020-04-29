75152ba41f40390d6af22134109a8d5455e266e0
cws <cws@home.fritz.box>
2020-04-29 15:49:39 +0200

    rm dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
